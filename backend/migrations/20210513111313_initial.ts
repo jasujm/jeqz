5ee47211cc4e44c49bca50f97ef635b8bf953c7f
Jaakko Moisio <jaakko@moisio.fi>
2021-05-29 14:49:18 +0300

    Add foreign key constraints

--- backend/migrations/20210513111313_initial.ts
+++ backend/migrations/20210513111313_initial.ts
@@ -7,30 +7,70 @@ export async function up(knex: Knex): Promise<void> {
     table.string("markup", 1023).notNullable();
     table.string("wikipediaId", 31).notNullable();
     table.dateTime("wikipediaTimestamp").notNullable();
-    table.timestamp("createdAt", { useTz: true }).notNullable().defaultTo(knex.fn.now());
-    table.timestamp("updatedAt", { useTz: true }).notNullable().defaultTo(knex.fn.now());
+    table
+      .timestamp("createdAt", { useTz: true })
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .timestamp("updatedAt", { useTz: true })
+      .notNullable()
+      .defaultTo(knex.fn.now());
   });
   await knex.schema.createTable("quizzes", (table) => {
     table.uuid("id").notNullable().primary();
-    table.timestamp("createdAt", { useTz: true }).notNullable().defaultTo(knex.fn.now());
-    table.timestamp("updatedAt", { useTz: true }).notNullable().defaultTo(knex.fn.now());
+    table
+      .timestamp("createdAt", { useTz: true })
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .timestamp("updatedAt", { useTz: true })
+      .notNullable()
+      .defaultTo(knex.fn.now());
   });
   await knex.schema.createTable("questions", (table) => {
     table.uuid("id").notNullable().primary();
-    table.uuid("quizId").notNullable();
+    table
+      .uuid("quizId")
+      .notNullable()
+      .references("id")
+      .inTable("quizzes")
+      .onDelete("CASCADE");
     table.integer("rank").notNullable();
-    table.timestamp("createdAt", { useTz: true }).notNullable().defaultTo(knex.fn.now());
-    table.timestamp("updatedAt", { useTz: true }).notNullable().defaultTo(knex.fn.now());
+    table
+      .timestamp("createdAt", { useTz: true })
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .timestamp("updatedAt", { useTz: true })
+      .notNullable()
+      .defaultTo(knex.fn.now());
     table.unique(["quizId", "rank"]);
   });
   await knex.schema.createTable("choices", (table) => {
     table.uuid("id").notNullable().primary();
-    table.uuid("questionId").notNullable();
-    table.uuid("equationId").notNullable();
+    table
+      .uuid("questionId")
+      .notNullable()
+      .references("id")
+      .inTable("questions")
+      .onDelete("CASCADE");
+    table
+      .uuid("equationId")
+      .notNullable()
+      .index()
+      .references("id")
+      .inTable("equations")
+      .onDelete("RESTRICT");
     table.boolean("isCorrect").notNullable();
     table.boolean("isSelected").nullable();
-    table.timestamp("createdAt", { useTz: true }).notNullable().defaultTo(knex.fn.now());
-    table.timestamp("updatedAt", { useTz: true }).notNullable().defaultTo(knex.fn.now());
+    table
+      .timestamp("createdAt", { useTz: true })
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .timestamp("updatedAt", { useTz: true })
+      .notNullable()
+      .defaultTo(knex.fn.now());
     table.unique(["questionId", "equationId"]);
   });
 }
